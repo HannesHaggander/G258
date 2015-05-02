1d47df0d4f76e9d022e606c707429d457dbfd4f8
dong <dongyingdong@hotmail.com>
2015-05-02 19:40:49 +0800

    testing

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,35 +6,57 @@ public class PlayerMovement : MonoBehaviour {
 
 	public Transform groundCheck;
 	public float jumpPower = 100;
-	public float speedX = 500;
 	public LayerMask whatIsGround;
 
 	private Rigidbody rb;
 	private PlayerInput PI;
 	private Vector3 jumpVector;
-	private Vector3 speedXVector;
 	private GameController GC;
+	private bool doublejump;
+	public Vector3 forwardforce;
+	public Vector3 maxspeed;
+	public Vector3 gravity;
+	public Vector3 defaultspeed;
+
 
 
 	public bool onGround = false;
 
 	// Use this for initialization
 	void Awake () {
+		doublejump = true;
 		rb = GetComponent<Rigidbody>();
 		PI = GetComponent<PlayerInput>();
 		jumpVector = new Vector3(0, jumpPower, 0);
-		speedXVector = new Vector3 (speedX, 0, 0);
-		rb.AddForce (speedXVector);
 		GC = GameObject.Find("_GAMECONTROLLER").GetComponent<GameController>();
-	
+		rb.AddForce (defaultspeed);
+
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+		onGround = Physics.Linecast(transform.position, groundCheck.transform.position, whatIsGround);
+		rb.AddForce (gravity);
+
+		if (onGround == true) {
+			doublejump = true;
+
+		} 
+
+
 		if(PI.jump && onGround){
 			rb.AddForce(jumpVector);
-
 		}
-		onGround = Physics.Linecast(transform.position, groundCheck.transform.position, whatIsGround);
+		if (onGround == false && PI.jump && doublejump) {
+			rb.AddForce(jumpVector);
+			doublejump =false;
+		
+		}
+		if (rb.velocity.x < maxspeed.x) {
+			rb.AddForce (forwardforce);
+		}
+
+
+
 	}
 }
