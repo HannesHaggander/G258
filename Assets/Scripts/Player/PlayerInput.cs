bc73e0013dcc38a184f9f187c2d24af654b10850
dong <dongyingdong@hotmail.com>
2015-05-06 05:09:15 +0800

    touch to jump enabled

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -8,9 +8,14 @@ public class PlayerInput : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		jump = Input.GetKeyDown(KeyCode.Space);
+		jump = false;
+		if (Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown (0)) {
+			jump = true;
+		}
+		//jump = Input.GetMouseButtonDown (0);
+		//jump = Input.GetKeyDown(KeyCode.Space);
 
-		if(Input.touchCount > 0){
+		if(Input.touchCount > 0 ){
 			jump = Input.GetTouch(0).phase.Equals(TouchPhase.Began);
 		
 		}
