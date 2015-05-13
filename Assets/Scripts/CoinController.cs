f7c811f26683bfdf9c4f84840e91f811e329c4dc
DzenoBa <DzenoBa@users.noreply.github.com>
2015-05-13 19:38:06 +0200

    + coins and score

---
+++ Assets/Scripts/CoinController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinController : MonoBehaviour {
+
+	private static int coinCount;// = 0;
+
+	// Use this for initialization
+	void Start () {
+		coinCount = 0;
+	}
+
+	void OnGUI() {
+		string coinText = "Score: " + coinCount;
+		GUI.Box (new Rect(Screen.width-150, 20, 130, 20), coinText);
+	}
+
+	public static void increaseCoins() {
+		coinCount = coinCount+1;
+	}
+}
---
+++ Assets/Scripts/CoinHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinHandler : MonoBehaviour {
+	
+	void OnTriggerEnter(Collider col){
+		Debug.Log (col.name + " entered ");
+		if(col.gameObject.name.Equals("Player") || col.gameObject.name.Equals("player")){
+			Debug.Log ("Coin");
+			Destroy(this.gameObject);
+			CoinController.increaseCoins();
+		}
+	}
+}
