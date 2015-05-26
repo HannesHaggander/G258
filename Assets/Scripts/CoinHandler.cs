4e0096a301fc7618854c109d07cfc1fb439cf06d
yooshen <frorgr@hotmail.com>
2015-05-26 08:42:44 +0200

    Fixed static variable problem in CoinController & CoinHandler

--- Assets/Scripts/CoinHandler.cs
+++ Assets/Scripts/CoinHandler.cs
@@ -3,12 +3,20 @@ using System.Collections;
 
 public class CoinHandler : MonoBehaviour {
 	
+	private CoinController coinController;
+
+	void Start() {
+		coinController = GameObject.Find ("CoinController").GetComponent<CoinController> ();
+	}
+
 	void OnTriggerEnter(Collider col){
 		Debug.Log (col.name + " entered ");
 		if(col.gameObject.name.Equals("Player") || col.gameObject.name.Equals("player")){
 			Debug.Log ("Coin");
 			Destroy(this.gameObject);
-			CoinController.increaseCoins();
+
+			//CoinController.increaseCoins();
+			coinController.increaseCoins();
 		}
 	}
 }
